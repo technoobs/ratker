60572bce2cd32d873ee5fb3a30c3f93aac9f3875
RobinChen <robinchen457@gmail.com>
2019-05-19 13:16:20 +1200

    Created initial infrastructure related files

---
+++ db_helper.sh
@@ -0,0 +1,5 @@
+# The helper script that is used to manage database.
+# -- Backup schema only: mysqldump --no-data -u someuser -p mydatabase
+# -- Backup data only: mysqldump --no-create-info --no-create-db
+
+#!bin/bash
