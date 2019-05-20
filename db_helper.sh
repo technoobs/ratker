1376b886e9e0238156d78294df06139fe59a46aa
RobinChen <robinchen457@gmail.com>
2019-05-20 21:00:18 +1200

    Renamed script to be simple text file, and add new command to dump data

--- db_helper.sh
+++
@@ -1,6 +0,0 @@
-# The helper script that is used to manage database.
-# -- Backup schema only: mysqldump --no-data -u someuser -p mydatabase
-# -- Backup data only: mysqldump --no-create-info --no-create-db
-
-#!bin/bash
-docker exec ratkerDevMaria /usr/bin/mysqldump -u root --password=password RatkerDev > sample.sql
